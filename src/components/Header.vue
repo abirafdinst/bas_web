d29bffb40239d9d13beacccf2c76fd22ad1ed073
abirafdinst <abirafdi13101@gmail.com>
2024-06-11 08:11:34 +0700

    first commit by abi

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <h1>
+            {{ text }}
+        </h1>
+        <slot />
+        <slot name="part1_header" :param1="data.param_from_slot"></slot>
+    </div>
+</template>
+<script setup>
+
+import { reactive } from 'vue';
+
+const data = reactive({
+    param_from_slot: "Hehehehe"
+})
+
+defineProps({
+    text: {
+        type: String,
+        default: " "
+    }
+})
+</script>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-responsive class="mx-auto" max-width="344">
+    <v-text-field hide-details="auto" label="id"></v-text-field>
+    <v-text-field hide-details="auto" label="account_id"></v-text-field>
+    <v-text-field hide-details="auto" label="bank_id"></v-text-field>
+    <v-text-field hide-details="auto" label="amount"></v-text-field>
+    <v-text-field hide-details="auto" label="transaction_time"></v-text-field>
+    
+  </v-responsive>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
